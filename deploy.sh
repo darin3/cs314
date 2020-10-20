25fa293a836caebbed407536b4986da76a035e48
Darin Harter <70183486+darin3@users.noreply.github.com>
2020-10-20 12:00:52 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
 # Author: Darin Harter
 # Email: darinh@cs.colostate.edu
 # GitHub Repo: https://github.com/darin3/cs314-deploy
 
-# Passwordless SSH login (optional): https://linuxize.com/post/how-to-setup-passwordless-ssh-login/
+# Password-less SSH login (optional): https://linuxize.com/post/how-to-setup-passwordless-ssh-login/
+# ** WARNING: This program will remove any "server-*.jar" files from your home directory on the remote server (for clean up). **
 
 : '
 MIT License
@@ -31,13 +31,11 @@ EXIT_SUCCESS=0
 EXIT_ERROR=1
 MACHINE="phoenix"
 
-if [ ! -d "bin/" ]
-then
+if [ ! -d "bin/" ]; then
   echo "Error: deploy.sh should be located in 'bin' and run with ./bin/deploy.sh"
   exit $EXIT_ERROR
 fi
 
-echo "** Warning: any server-#.jar files in the home directory of the remote machine will be deleted (for clean up). **"
 echo -n "eID: "
 read -r eid
 echo -n "Target (DEV or SPRINT#): "
@@ -49,26 +47,24 @@ if [[ ! $server =~ ^(DEV|SPRINT[1-5])$ ]]; then
 fi
 
 rebuild="y"
-if [ -d "target" ]; then
-  # only give option to not rebuild if target directory exists
+if (ls target/server-*.jar >/dev/null); then
+  # give option to not rebuild if jar already exists in target directory
   echo -n "Rebuild jar? (recommended) [y/n]: "
-  read -r rebuild
+  read -r rebuild;
 fi
 
-if [[ $rebuild =~ ^[yY]$ ]]; then
+if [[ $rebuild =~ ^[yY]|yes|Yes|YES$ ]]; then
   echo "Building project with './run.sh -d'"
-  if ! ./bin/run.sh -d >/dev/null 2>&1
-  then
-    printf "Error when executing command: '%s'\n" "$0"
-    printf "  Run './bin/run.sh -d' normally to check for errors or failed tests.\n"
-    printf "  Reminder: set up port forwarding if needed.\n"
+  if ! (./bin/run.sh -d >/dev/null 2>&1); then
+    echo "Error occurred during run script."
+    echo "  Run './bin/run.sh -d' normally to check for errors or failed tests."
+    echo "  Reminder: set up port forwarding if needed."
     exit $EXIT_ERROR
   fi
 fi
 
 echo "Copying server-#.jar to CSU machine."
-if ! rsync target/server-*.jar "${eid}@${MACHINE}.cs.colostate.edu:"
-then
+if ! (rsync target/server-*.jar "${eid}@${MACHINE}.cs.colostate.edu:"); then
   echo "Error: rsync failed, could not copy jar to remote machine."
   exit $EXIT_ERROR
 fi
@@ -76,8 +72,8 @@ fi
 echo "Running \"~cs314/bin/checkin ${server} server-*.jar\""
 echo "Deleting jar file."
 
-if ! ssh "${eid}@${MACHINE}.cs.colostate.edu" "~cs314/bin/checkin ${server} server-*.jar; rm server-*.jar"
-then
+CHECKIN_AND_RM_JAR="~cs314/bin/checkin ${server} server-*.jar; rm server-*.jar"
+if ! (ssh "${eid}@${MACHINE}.cs.colostate.edu" "${CHECKIN_AND_RM_JAR}"); then
   echo "Error: there was a problem with ssh or ~cs314/bin/checkin."
   exit $EXIT_ERROR
 fi
